3dc0a1ca30e39ce3fe698578a960d11b6de22e15
Joshua Lavarine <jlavarine@plos.org>
2026-01-27 13:09:48 +0000

    Revert "Add custom submission POC"
    This reverts commit 4b0636bb58a069d70552ad2b92b909f20a0a830a

--- customsubmission/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-from .views import FORMS
-
-urlpatterns = [
-    path('submit/custom/', views.CustomSubmissionWizard.as_view(FORMS), name='custom_submission_wizard'),
-]
