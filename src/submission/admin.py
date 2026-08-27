ef0ecf5de7908c671a915ba3210cfe994addfa0c
Rose Reatherford <rreatherford@plos.org>
2026-08-27 09:45:54 -0500

    Update to allow choices to be managed for additional fields such that there is better ordering as well as actual vs display value options.
    Jira: RCT-731

--- src/submission/admin.py
+++ src/submission/admin.py
@@ -3,12 +3,12 @@ __author__ = "Martin Paul Eve & Andy Byers"
 __license__ = "AGPL v3"
 __maintainer__ = "Birkbeck Centre for Technology and Publishing"
 
-from django.contrib import admin
 from django import forms
+from django.contrib import admin
 from django.template.defaultfilters import truncatewords_html
 
-from utils import admin_utils
 from submission import models
+from utils import admin_utils
 
 
 class LicenseChoiceField(forms.ModelChoiceField):
@@ -262,10 +262,17 @@ class FieldSectionInline(admin.TabularInline):
     ordering = ("order",)
 
 
+class FieldChoiceInline(admin.TabularInline):
+    model = models.FieldChoice
+    extra = 1
+    ordering = ("order",)
+
+
 class FieldAdmin(admin.ModelAdmin):
     list_display = ("name", "journal", "press", "kind", "width", "required", "display")
     list_filter = ("journal", "press", "kind", "width", "required", "display")
     search_fields = ("pk", "name", "help_text", "choices")
+    inlines = [FieldChoiceInline]
 
 
 class FieldAnswerAdmin(admin_utils.ArticleFKModelAdmin):
@@ -274,7 +281,7 @@ class FieldAnswerAdmin(admin_utils.ArticleFKModelAdmin):
     search_fields = ("pk", "field__name", "article__pk", "article__title", "answer")
 
     def _answer(self, obj):
-        return truncatewords_html(obj.answer, 10) if obj else ""
+        return truncatewords_html(obj.display_value, 10) if obj else ""
 
 
 class SubmissionConfigAdmin(admin.ModelAdmin):
